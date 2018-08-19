6c3be4fbb22d44608ab91f88e615c11a1ba5c8cd
InTheSecondHalf <33090896+sangdo913@users.noreply.github.com>
2018-08-19 12:22:42 +0900

    Update 주사위굴리기.cpp

--- 12주차/과제/이상도/주사위굴리기.cpp
+++ 12주차/과제/이상도/주사위굴리기.cpp
@@ -1,94 +1,97 @@
-﻿#include<iostream>
-#include<cstring>
-#include<cstdio>
+#include<iostream>
 
 using namespace std;
 
-int map[50][50];
-const int dr[4] = { -1,1,0,0 };
-
-int n, m;
-const int dc[4] = { 0,0,-1,1 };
-const int change[4][4] = { { 2,1,3,0 },{ 0,2,1,3 },{ 3,0,2,1 },{ 1,3,0,1 } };
-const int UP = 0;
-const int RIGHT = 1;
-const int DOWN = 2;
-const int LEFT = 3;
-struct Cleaner
+struct RollDice_Dice
 {
-	int direct;
-	int r, c;
-	int clean;
-	int check()
-	{
-		int nextR = r + dr[change[direct][0]];
-		int nextC = c + dc[change[direct][0]];
+    int n; int m;
+    int up,down,left,right,front,rear;
+    int map[22][22];
+    int positionX;
+    int positionY;
+    
+    const int dx[5] = {0,1,-1,0,0};
+    const int dy[5] = {0,0,0,-1,1};
+    RollDice_Dice()
+    {
+        for(int i =0;i<22;i++)
+        {
+            for(int j = 0 ; j<22; j++) map[i][j] = -1;
+        }
+        down = left = right = front = rear = up = 0;
+    }
+    int print()
+    {
+       cout << up <<'\n'; 
+    }
+    
+    void change()
+    {
+        if(map[positionY][positionX])
+        {
+            down = map[positionY][positionX];
+            map[positionY][positionX] = 0;
+        }
+        else
+        {
+            map[positionY][positionX] = down;
+        }
+    }
+    bool roll(int d)
+    {
+        int nextX = positionX + dx[d], nextY = positionY + dy[d];
+        
+        if(map[nextY][nextX] == -1) return false ;
+        positionX = nextX;
+        positionY = nextY;
+        int temp;
+        temp = up;
+        switch(d)
+        {
+            case 1:
+            up = left; left = down; down = right;
+            right = temp;
+            break;
+            case 2:
+            up = right; right = down; down = left;
+            left = temp;
+            break;
+            case 3:
+            up = front; front = down; down = rear;
+            rear = temp;
+            break;
+            case 4:
+            up = rear; rear = down; down = front;
+            front = temp;
+            break;
 
-		bool dirty = false;
 
-		for (int i = 0; i < 4; i++)
-		{
-			nextR = r + dr[i];
-			nextC = c + dc[i];
+        }
+        change();
+        print();
+    }
+} RollDice_dice;
 
-			dirty |= map[nextR][nextC] == 0;
-		}
-
-		if (!dirty)
-		{
-			nextR = r + dr[change[direct][1]];
-			nextC = c + dc[change[direct][1]];
-			if (map[nextR][nextC] == 1) return -1;
-			r = nextR; c = nextC;
-			return 2;
-		}
-
-		nextR = r + dr[change[direct][0]];
-		nextC = c + dc[change[direct][0]];
-
-		if (map[nextR][nextC] == 0)
-		{
-			r = nextR;
-			c = nextC;
-			direct = (direct + 3) % 4;
-			return 1;
-		}
-
-		direct = (direct + 3) % 4;
-		return 2;
-	}
-	int process()
-	{
-		while (true)
-		{
-			if (map[r][c] == 0)
-			{
-				map[r][c] = 2;
-				clean++;
-			}
-
-
-			while (true)
-			{
-				int flag = check();
-				if (flag == 1) break;
-				if (flag == -1) return clean;
-
-			}
-		}
-	}
-} cleaner;
 int main()
 {
-	scanf("%d %d\n", &n, &m);
-	scanf("%d %d %d\n", &cleaner.r, &cleaner.c, &cleaner.direct);
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++)
-			scanf("%d", &map[i][j]);
-	}
-
-	int res = cleaner.process();
-	printf("%d\n", res);
-	return 0;
+    int k;
+    int **map = (int**)RollDice_dice.map;
+    int&n = RollDice_dice.n;
+    int &m = RollDice_dice.m;
+    cin >> RollDice_dice.n >> RollDice_dice.m >> RollDice_dice.positionY >> RollDice_dice.positionX >> k;
+    RollDice_dice.positionX++; RollDice_dice.positionY++;
+    for(int i = 1 ; i<=n;i++)
+    {
+        for(int j = 1 ; j<=m; j++)
+        {
+            cin >> RollDice_dice.map[i][j];
+        }
+    }
+    int comm;
+    for(int i  = k;i--;)
+    {
+        cin >> comm;
+        RollDice_dice.roll(comm);
+    }
+    return 0;
 }

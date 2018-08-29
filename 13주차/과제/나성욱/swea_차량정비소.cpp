540d1ac2e742d85bef86338fa1531a17cb2426f8
NABI\skqls_000 <skqlsjcm312>
2018-08-29 16:56:57 +0900

    modified:   "swea_\354\260\250\353\237\211\354\240\225\353\271\204\354\206\214.cpp"

--- 13주차/과제/나성욱/swea_차량정비소.cpp
+++ 13주차/과제/나성욱/swea_차량정비소.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 typedef struct Person {
 	int cusnum = 0;
-	int time  = -1;
+	int time = -1;
 	int rec = 0;
 	int rep = 0;
 }P;
@@ -21,8 +21,13 @@ vector <vector <P>> cus_list;
 void Init() {
 	queue <P> t;
 	rec_waiting = rep_waiting = t;
+
+
+	for (int i = 0; i < cus_list.size(); i++) {
+		cus_list[i].clear();
+	}
 	cus_list.clear();
-	
+
 	for (int i = 0; i < 10; i++) {
 		rec_ability[i] = 0;
 		rep_ability[i] = 0;
@@ -31,17 +36,17 @@ void Init() {
 
 void Get() {
 	int temp = 0;
-	
+
 	scanf("%d %d %d %d %d", &n, &m, &k, &rec_target, &rep_target);
 
 	for (int i = 1; i <= n; i++) {
-		scanf("%d", rec_ability+i);
+		scanf("%d", rec_ability + i);
 	}
 	for (int i = 1; i <= m; i++) {
 		scanf("%d", rep_ability + i);
 	}
 
-	cus_list.resize(1001);
+	cus_list.resize(3000);
 
 	for (int i = 1; i <= k; i++) {
 		scanf("%d", &temp);
@@ -61,25 +66,20 @@ int Solution() {
 	vector <P> rep_service(10);
 	vector <P> finished;
 
+	for (int i = 0; i < 3000; i++) {
 
-	
-	for (int i = 0; i < 1001; i++) {
-		
 		for (int j = 0; j < cus_list[i].size(); j++) {
 			rec_waiting.push(cus_list[i][j]);
 		}
-		
-		
-		
+
 		for (int j = 1; j <= n; j++) {
-			
 
 			if (--rec_service[j].time <= 0) {
 				if ((rec_service[j].cusnum > 0) && (rec_service[j].time == 0)) {
 					rep_waiting.push(rec_service[j]);
 				}
 				if (!rec_waiting.empty()) {
-					rec_service[j] = rec_waiting.front();
+					rec_service[j].cusnum = rec_waiting.front().cusnum;
 					rec_service[j].rec = j;
 					rec_service[j].time = rec_ability[j];
 					rec_waiting.pop();
@@ -88,14 +88,14 @@ int Solution() {
 		}
 
 		for (int j = 1; j <= m; j++) {
-			
 
-			if (--rep_service[j].time <= 0 ) {
-				if ((rep_service[j].cusnum > 0) && (rep_service[j].time == 0)){
+			if (--rep_service[j].time <= 0) {
+				if ((rep_service[j].cusnum > 0) && (rep_service[j].time == 0)) {
 					finished.push_back(rep_service[j]);
 				}
 				if (!rep_waiting.empty()) {
-					rep_service[j] = rep_waiting.front();
+					rep_service[j].cusnum = rep_waiting.front().cusnum;
+					rep_service[j].rec = rep_waiting.front().rec;
 					rep_service[j].rep = j;
 					rep_service[j].time = rep_ability[j];
 					rep_waiting.pop();
@@ -106,13 +106,14 @@ int Solution() {
 			break;
 	}
 
+	//cout << "finished SIZE : " << finished.size() << '\n';
 	for (int i = 0; i < finished.size(); i++) {
 		if ((finished[i].rec == rec_target) && (finished[i].rep == rep_target)) {
-		//	cout << finished[i].cusnum << '\n';
+			//	cout << finished[i].cusnum << '\n';
 			ret += finished[i].cusnum;
 		}
 	}
-	
+
 	return (ret == 0) ? -1 : ret;
 }
 
